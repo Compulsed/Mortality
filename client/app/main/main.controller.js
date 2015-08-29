8420b8ac6b1dac92d0ab5ca6ae7f839bf3f20556
Dale Salter <djsalter93@hotmail.com>
2015-08-29 19:52:45 +1000

    FIXED: Moment timing bug

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,18 +1,31 @@
 'use strict';
 
+var Entry = function Entry (mortality) {
+  this.name = mortality.name;
+  this.dateAdded = mortality.dateAdded;
+  this.inputDate = mortality.inputDate;
+  this.daysAlive = mortality.daysAlive;
+}
+
+Entry.prototype.calculate = function() {
+  this.inputDate = moment(this.inputDate).format('MMMM Do YYYY');
+
+  this.dateAdded = moment(this.dateAdded).fromNow();
+}
+
 angular.module('mortalityApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.mortalityTimes = [];
     $scope.error = false;
 
-    $http.get('/api/mortality').success(function(mortalityTimes) {
-      mortalityTimes.forEach(function(time) {
-          time.inputDate = moment(time.inputDate).format('MMMM Do YYYY');
-          time.dateAdded = moment(time.dateAdded).startOf('hour').fromNow();
+    $http.get('/api/mortality').success(function(mortalityEntries) {
+      mortalityEntries.forEach(function(mortalityEntry) {
+          var entry = new Entry(mortalityEntry);
+          entry.calculate(); // Converts to a readable format
+          $scope.mortalityTimes.push(entry);
       });
 
-      mortalityTimes.reverse();
-      $scope.mortalityTimes = mortalityTimes;
+      $scope.mortalityTimes.reverse();
     });
 
     $scope.addThing = function() {
@@ -22,26 +35,22 @@ angular.module('mortalityApp')
 
       var enteredDate = new Date($scope.dateOfBirth);
       var currentDate = new Date();
-
-      // Found here: http://stackoverflow.com/questions/2627473/how-to-calculate-the-number-of-days-between-two-dates-using-javascript
       var daysAlive = Math.round(
         (currentDate.getTime() - enteredDate.getTime())/(24*60*60*1000)
       )
 
       if (daysAlive >= 0)
       {
-        var newEntry = { 
-            name: $scope.name,
-            dateAdded: currentDate,
-            inputDate: enteredDate,
-            daysAlive: daysAlive 
-        };
-
+        var newEntry = new Entry(
+          {
+              name: $scope.name,
+              dateAdded: currentDate,
+              inputDate: enteredDate,
+              daysAlive: daysAlive
+          });
 
         $http.post('/api/mortality', newEntry).then(function(response){
-          newEntry.inputDate = moment(newEntry.inputDate).format('MMMM Do YYYY');
-          newEntry.dateAdded = moment(newEntry.dateAdded).startOf('hour').fromNow();
-
+          newEntry.calculate();
           $scope.mortalityTimes.unshift(newEntry);
         });
 
